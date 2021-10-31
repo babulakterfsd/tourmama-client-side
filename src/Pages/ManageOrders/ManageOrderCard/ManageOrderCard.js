968782be241d1cc3e8f7ed4e3bac84e716224dd8
Babul Akter <babulakterfsd@gmail.com>
2021-10-31 12:03:59 +0600

    clients detail shown on manage all order page

--- src/Pages/ManageOrders/ManageOrderCard/ManageOrderCard.js
+++ src/Pages/ManageOrders/ManageOrderCard/ManageOrderCard.js
@@ -5,21 +5,17 @@ import "./ManageOrderCard.css";
 
 const SinglepackageCard = ({ detail }) => {
 
-  console.log(detail);
-
   const history = useHistory();
-    const redirect_uri = "/";
-  
+  const redirect_uri = "/";
 
   //delete a package
   const handleDeletePack = (id) => {
     const proceed = window.confirm("Are you sure to delete the user?");
     if (proceed) {
-      
       const url = `https://radiant-shore-89532.herokuapp.com/allorder/${id}`;
 
       fetch(url, {
-        method: "DELETE"
+        method: "DELETE",
       })
         .then((res) => res.json())
         .then((data) => {
@@ -36,22 +32,20 @@ const SinglepackageCard = ({ detail }) => {
     const url = `https://radiant-shore-89532.herokuapp.com/allorder/${id}`;
 
     fetch(url, {
-        method: 'PUT',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(detail)
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(detail),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.modifiedCount > 0) {
-                alert('Order Approved !');
-                history.push(redirect_uri)
-            }
-        })
-}
-
- 
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          alert("Order Approved !");
+          history.push(redirect_uri);
+        }
+      });
+  };
 
   return (
     <Col>
@@ -69,16 +63,60 @@ const SinglepackageCard = ({ detail }) => {
             <Card.Text className="text-light-green fw-semi-bold">
               {detail?.order?.duration}
             </Card.Text>
-            <h4 className="text-light-green fw-bolder">${detail?.order?.price}</h4>
+            <h4 className="text-light-green fw-bolder">
+              ${detail?.order?.price}
+            </h4>
           </div>
 
           <Card.Title className="abril-font">{`${detail?.order?.location?.city}, ${detail?.order?.location?.country}`}</Card.Title>
-          <p className="text-cyan">we provide: {detail?.order?.services.join(", ")}</p>
-          <span className="text-danger fw-bolder"> <span className="text-cyan fw-semi-bolder">Order Status: </span> {detail?.status}</span>
+          <p className="text-cyan">
+            we provide: {detail?.order?.services.join(", ")}
+          </p>
+
+          <h6 className="text-danger fw-bolder">
+            {" "}
+            <span className="text-cyan fw-semi-bolder">
+              Order Status:{" "}
+            </span>{" "}
+            {detail?.status}
+          </h6>
         </Card.Body>
+        <Card.Footer>
+          <div className="client-info p-2">
+            <h6 className="text-cyan">
+              {" "}
+              <span className="fw-semi-bold">Client Name: </span> {detail?.name}
+            </h6>
+            <h6 className="text-cyan">
+              {" "}
+              <span className=" fw-semi-bold">Clients Email: </span>{" "}
+              {detail?.email}
+            </h6>
+            <h6 className="text-cyan">
+              {" "}
+              <span className=" fw-semi-bold">Clients Phone: </span>{" "}
+              {detail?.phone}
+            </h6>
+            <h6 className="text-cyan">
+              {" "}
+              <span className=" fw-semi-bold">Clients Address: </span>{" "}
+              {detail?.address}
+            </h6>
+          </div>
+        </Card.Footer>
         <Card.Footer className="d-flex justify-content-between">
-            <Button onClick={() => handleDeletePack(detail?._id)} className="btn-danger text-white  py-2 px-3">Cancel Order</Button>
-            <Button onClick={() => handleUpdate(detail?._id)} className="btn-light-green py-2 px-3">Approve Order</Button>
+          <Button
+            onClick={() => handleDeletePack(detail?._id)}
+            className="btn-danger text-white  py-2 px-3"
+          >
+            Cancel Order
+          </Button>
+          <Button
+            onClick={() => handleUpdate(detail?._id)}
+            className="btn-light-green py-2 px-3"
+          >
+            Approve Order
+          </Button>
         </Card.Footer>
       </Card>
     </Col>
